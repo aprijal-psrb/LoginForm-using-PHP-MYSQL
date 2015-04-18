0155f712551bfc06cd2133e578c5a3c96add06e4
Aprijal Pasaribu <aprijal.psrb@gmail.com>
2015-04-18 12:51:35 +0700

    add class AppConfig

---
+++ app/src/main/java/com/asa/loginform_php_mysql/AppConfig.java
@@ -0,0 +1,12 @@
+package com.asa.loginform_php_mysql;
+
+/**
+ * Created by APRIJAL_PASARIBU on 18/04/2015.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.173.1/asa/android/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.173.1/asa/android";
+}
